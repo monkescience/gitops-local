55cda53df172a312eec427c5d4b68b517ecd45ae
ValentinLutz <allround.email@gmail.com>
2025-12-27 10:47:26 +0100

    chore: add cross-cluster configuration to Istio manifests and update gateway infrastructure parameters

--- scripts/lib/network.sh
+++ scripts/lib/network.sh
@@ -49,5 +49,6 @@ get_cluster_ip_on_network() {
   local cluster_name=$1
   local container_name="$cluster_name-control-plane"
 
-  docker inspect "$container_name" --format "{{(index .NetworkSettings.Networks \"$NETWORK_NAME\").IPAddress}}" 2>/dev/null
+  # Use 'kind' network IP as it's included in the API server certificate SANs
+  docker inspect "$container_name" --format '{{(index .NetworkSettings.Networks "kind").IPAddress}}' 2>/dev/null
 }
